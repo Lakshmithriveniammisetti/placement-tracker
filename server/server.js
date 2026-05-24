0470d000dca96b59e1f83527f0c9ba0ec0a83499
Lakshmithriveniammisetti <thriveniammisetty@gmail.com>
2026-05-24 12:17:15 +0530

    fixed cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "https://placement-tracker-frontend.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
